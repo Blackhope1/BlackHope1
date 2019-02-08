213c2dd6ea9da4767f96ea951c0c10fb7cfa7b8e
Blackhope1 <44577597+Blackhope1@users.noreply.github.com>
2019-02-08 17:23:04 +0100

    Add files via upload

---
+++ FortranProjects/Armonico/Debug/hello_world__genmod.f90
@@ -0,0 +1,9 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sun Dec 16 19:06:45 2018
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE HELLO_WORLD__genmod
+          INTERFACE 
+            SUBROUTINE HELLO_WORLD
+            END SUBROUTINE HELLO_WORLD
+          END INTERFACE 
+        END MODULE HELLO_WORLD__genmod
